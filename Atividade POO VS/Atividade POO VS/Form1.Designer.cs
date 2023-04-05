975d4e1849e9532390d6c5433b093a4b40b30626
Fernanda019 <fernanda.marcelino2@etec.sp.gov.br>
2023-04-05 18:26:15 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade POO VS/Atividade POO VS/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Atividade_POO_VS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblano = new Label();
+            lblnomes = new Label();
+            lbltitulo = new Label();
+            SuspendLayout();
+            // 
+            // lblano
+            // 
+            lblano.AutoSize = true;
+            lblano.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblano.Location = new Point(283, 246);
+            lblano.Name = "lblano";
+            lblano.Size = new Size(236, 32);
+            lblano.TabIndex = 6;
+            lblano.Text = "2º Jogos Digitais";
+            // 
+            // lblnomes
+            // 
+            lblnomes.AutoSize = true;
+            lblnomes.Font = new Font("Arial", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lblnomes.Location = new Point(199, 214);
+            lblnomes.Name = "lblnomes";
+            lblnomes.Size = new Size(403, 32);
+            lblnomes.TabIndex = 5;
+            lblnomes.Text = "Denis Rocha e Fernanda Viana";
+            // 
+            // lbltitulo
+            // 
+            lbltitulo.AutoSize = true;
+            lbltitulo.Font = new Font("Arial Narrow", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbltitulo.Location = new Point(283, 172);
+            lbltitulo.Name = "lbltitulo";
+            lbltitulo.Size = new Size(220, 42);
+            lbltitulo.TabIndex = 4;
+            lbltitulo.Text = "Atividade POO";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblano);
+            Controls.Add(lblnomes);
+            Controls.Add(lbltitulo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblano;
+        private Label lblnomes;
+        private Label lbltitulo;
+    }
+}
